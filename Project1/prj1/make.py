9b59b135a3cae98af75b5960e48b3237619c0762
GioPede <giovanni.pederiva@gmail.com>
2015-10-05 17:53:32 +0200

    Project 2

--- Project1/prj1/make.py
+++ Project1/prj1/make.py
@@ -1,7 +1,7 @@
 import os 
 
 print "compiling...\n"
-os.system("g++ -c -Wall -fPIC -I. -I/local/include/python2.4 -c  main.cpp")
+os.system("g++ -c -Wall -fPIC -I. -O3 -I/local/include/python2.4 -c  main.cpp")
 print "linking...\n"
 os.system("g++ -o  prj1 lib.o main.o  ")
 print "running...\n"
